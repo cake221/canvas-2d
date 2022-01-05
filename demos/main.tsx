e0ed11042b8d730168e8a01adaded1283c8a3fb2
wenyufei <wenyufei@aliyun.com>
2022-01-05 21:50:22 +0800

    workflow: 重新触发构建

--- demos/main.tsx
+++ demos/main.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./src/App"
 import "../node_modules/antd/dist/antd.css"
+import App from "./src/App"
 
 ReactDOM.render(
   <React.StrictMode>
