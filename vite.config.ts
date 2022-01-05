85b52b47cd48443ce289644afc4256adebeb5e4f
wenyufei <wenyufei@aliyun.com>
2022-01-05 22:00:50 +0800

    workflow: vite bulid，生成环境添加base路径

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "./demos",
-  plugins: [react()]
+  plugins: [react()],
+  base: process.env.NODE_ENV === "production" ? "/canvas-2d/" : "./"
 })
